2f234193ade0803de1818d3e92ef99e47e00976f
Utkarsh0203 <ag.utkarsh0203@gmail.com>
2021-04-28 21:23:42 +0530

    changes to homepage

--- controllers/home.js
+++ controllers/home.js
@@ -4,17 +4,17 @@ exports.get_home = (req,res2,next) => {
     // get_history ???
     // get_followers, get_following
     // get_all_users
-    console.log("get_home*************************************");
-    console.log(req.query.queryuser);
-    if(req.query.queryuser){
-        console.log("queryuser");
-    }
+    // console.log("get_home*************************************");
+    // console.log(req.query.queryuser);
+    // if(req.query.queryuser){
+    //     console.log("queryuser");
+    // }
     if(req.session.user){
         const home_obj = new db.User(req.session.user);
         home_obj.get_all_users()
             .then(res =>{
                 const users_list = res.records;
-                req.session.users_list = users_list;
+                // req.session.users_list = users_list;
                 console.log(users_list[0]._fields);
                 console.log("userlist");
                 home_obj.fetch_info()
@@ -24,35 +24,60 @@ exports.get_home = (req,res2,next) => {
                         home_obj.get_liked_movies()
                             .then(res =>{
                                 const liked_movies = res.records[0];
-                                req.session.liked_movies = liked_movies;
+                                // req.session.liked_movies = liked_movies;
                                 console.log(liked_movies);
                                 home_obj.get_disliked_movies()
                                     .then(res =>{
                                         const disliked_movies = res.records[0];
-                                        req.session.disliked_movies = disliked_movies;
+                                        // req.session.disliked_movies = disliked_movies;
                                         console.log(disliked_movies);
                                         home_obj.get_followers()
                                             .then(res =>{
-                                                const followers_list = res.records[0];
-                                                req.session.followers_list = followers_list;
-                                                console.log(followers_list);
+                                                const followers_list = res.records;
+                                                // req.session.followers_list = followers_list;
+                                                console.log("followers_list");
+                                                // console.log(followers_list);
                                                 home_obj.get_following()
                                                     .then(res =>{
-                                                        const following_list = res.records[0];
-                                                        req.session.following_list = following_list;
+                                                        const following_list = res.records;
+                                                        // req.session.following_list = following_list;
                                                         console.log(following_list);
-                                                        res2.render('home', {
-                                                            pageTitle: 'Home Page',
-                                                            path: '/home/?username='+req.session.user,
-                                                            username: req.session.user,
-                                                            liked_movies: liked_movies,
-                                                            disliked_movies: disliked_movies,
-                                                            users_list: users_list,
-                                                            followers_list: followers_list,
-                                                            following_list: following_list,
-                                                            friend_data: undefined,
-                                                            is_following: undefined
-                                                        });
+                                                        if(req.query.queryuser){
+                                                            const friend_obj = new db.Friends(req.session.user);
+                                                            const friend_id = req.query.queryuser;
+                                                            friend_obj.fetch_info(friend_id)
+                                                                .then(res =>{
+                                                                    const friend_data = res.records[0];
+                                                                    console.log("friend_data&&&&&&&&&&");
+                                                                    console.log(friend_data)
+                                                                    res2.render('home', {
+                                                                        pageTitle: 'Home Page',
+                                                                        path: '/home/?username='+req.session.user+'&queryuser='+friend_id+'&follows='+req.query.follows,
+                                                                        username: req.session.user,
+                                                                        liked_movies: liked_movies,
+                                                                        disliked_movies: disliked_movies,
+                                                                        users_list: users_list,
+                                                                        followers_list: followers_list,
+                                                                        following_list: following_list,
+                                                                        friend_data: friend_data,
+                                                                        is_following: req.query.follows
+                                                                    });
+                                                                })
+                                                        }
+                                                        else{
+                                                            res2.render('home', {
+                                                                pageTitle: 'Home Page',
+                                                                path: '/home/?username='+req.session.user,
+                                                                username: req.session.user,
+                                                                liked_movies: liked_movies,
+                                                                disliked_movies: disliked_movies,
+                                                                users_list: users_list,
+                                                                followers_list: followers_list,
+                                                                following_list: following_list,
+                                                                friend_data: undefined,
+                                                                is_following: undefined
+                                                            });
+                                                        }
                                                     })
                                             })
                                     })
@@ -75,15 +100,11 @@ exports.post_search_friend = (req,res2,next) => {
     const friend_id = req.body.search_username;
     const userid = req.session.user;
     console.log(friend_id);
-    friend_obj.fetch_info(friend_id)
+    friend_obj.is_following(friend_id)
         .then(res =>{
-            const friend_data = res.records[0];
-            console.log(friend_data)
-            friend_obj.is_following(friend_id)
-                .then(res =>{
-                        const is_following = res.records[0]._fields[0];
-                        console.log(is_following);
-                        res2.redirect('/home/?username='+req.session.user+'&?queryuser='+friend_id);
+                const is_following = res.records[0]._fields[0];
+                console.log(is_following);
+                res2.redirect('/home/?username='+req.session.user+'&queryuser='+friend_id+'&follows='+is_following);
                         // res2.render('home', {
                         //     pageTitle: 'Home Page',
                         //     path: '/home/?username='+req.session.user,
@@ -96,7 +117,7 @@ exports.post_search_friend = (req,res2,next) => {
                         //     friend_data: friend_data,
                         //     is_following: is_following
                         // });
-                    })
+                    // })
         })
 };
 
