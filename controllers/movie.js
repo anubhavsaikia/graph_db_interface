580b64ced565e3bedfa360fb3c145ee5b9511fb7
Rhushabh1 <rhushabhgedam3@gmail.com>
2021-05-07 03:36:37 +0530

    added search movies

--- controllers/movie.js
+++ controllers/movie.js
@@ -5,30 +5,36 @@ exports.get_movie = (req,res2,next) => {
 
     console.log("search movie");
     const movie_obj =  new db.Movie(req.query.movieid);
+    
     movie_obj.get_all_movies()
-        .then(res => {
-            const movie_list = res.records;
-            req.session.movie_list = movie_list;
-            console.log(movie_list[0]._fields);
-            console.log("movielist");
-            if (req.session.user){
-                res2.render('movie', {
-                    pageTitle: 'Movie Data',
-                    path: '/movie',
-                    movie_list: movie_list,
-                    username: req.session.user,
-                    is_logged_in: true
-                });
-            } else{
-                res2.render('movie', {
-                    pageTitle: 'Movie Data',
-                    path: '/movie',
-                    movie_list: movie_list,
-                    username: req.session.user,
-                    is_logged_in: false
-                });
-            }
-        })
+    .then(res => {
+        const movie_list = res.records;
+        // req.session.movie_list = movie_list;
+        console.log(movie_list[0]._fields);
+        console.log("movielist");
+        if (req.session.user){
+            req.session.is_logged_in = true;
+            res2.render('movie', {
+                pageTitle: 'Movie Data',
+                path: '/movie',
+                movie_list: movie_list,
+                username: req.session.user,
+                is_logged_in: req.session.is_logged_in,
+                movie_data: false
+            });
+        } else{
+            req.session.is_logged_in = false;
+            res2.render('movie', {
+                pageTitle: 'Movie Data',
+                path: '/movie',
+                movie_list: movie_list,
+                username: req.session.user,
+                is_logged_in: req.session.is_logged_in,
+                movie_data: false
+            });
+        }
+    })
+    
     
 };
 
@@ -36,61 +42,98 @@ exports.post_search = (req,res2,next) => {
     // fetch_movie
     // get_rating
     console.log("post_search_movie");
-    const movie_obj = new db.Movie(req.body.search_moviename);
-    console.log(req.body.movie)
-
-    const imdb_id = req.body.movie_id;
-    req.session.movie_id = imdb_id;
+    const imdb_id = req.body.search_movieid;
     const userid = req.session.user;
+    const movie_obj = new db.Movie(imdb_id);
+    const user_obj = new db.User(userid);
+    // console.log(req.body.movie)
+
+    req.session.movie_id = imdb_id;
     console.log(imdb_id);
 
-    db.Movie.fetch_movie(imdb_id, userid)
+    movie_obj.fetch_movie()
+    .then(res => {
+        const movie_data = res.records;
+        // req.session.movie_id = imdb_id;
+        req.session.movie_data = movie_data;
+        console.log(movie_data);
+        movie_obj.get_all_movies()
         .then(res => {
-            const movie_data = res.rows;
-            req.session.movie_id = imdb_id;
-            req.session.movie_data = movie_data;
-            movies.Movie.get_rating(imdb_id, userid)
+            const movie_list = res.records;
+            console.log(movie_list[0]._fields);
+            console.log("movielist");
+                
+            if (req.session.user){
+                user_obj.get_rating(imdb_id)
                 .then(res => {
-                    const user_rating = res.rows;
+                    const user_rating = res.records[0];
+                    console.log(user_rating);
                     res2.render('movie', {
                         pageTitle: 'Movie Data',
                         path: '/movie/?movie_id='+imdb_id,
-                        movie_data: movie_data,
-                        user_rating: user_rating
+                        movie_data: req.session.movie_data,
+                        user_rating: user_rating,
+                        movie_list: movie_list,
+                        username: req.session.user,
+                        is_logged_in: req.session.is_logged_in
                     });
                 })
+            } else{
+                res2.render('movie', {
+                    pageTitle: 'Movie Data',
+                    path: '/movie/?movie_id='+imdb_id,
+                    movie_data: req.session.movie_data,
+                    user_rating: false,
+                    movie_list: movie_list,
+                    username: req.session.user,
+                    is_logged_in: req.session.is_logged_in
+                });
+            }
         })
+    })
 };
-
-// <% if(is_logged_in == true){ %>
-//     <h3>MovieID : <%= qmovie[0]._fields[0].properties.movieid %></h3> 
-//     <h3>Title : <%= qmovie[0]._fields[0].properties.title %></h3> 
-//     <h3>Year : <%= qmovie[0]._fields[0].properties.year %></h3> 
-// <% };%>
-// <% if(is_logged_in == false){ %>
-//     <h3>Can't Rate</h3> 
-//     <h3>MovieID : <%= qmovie[0]._fields[0].properties.movieid %></h3> 
-//     <h3>Title : <%= qmovie[0]._fields[0].properties.title %></h3> 
-//     <h3>Year : <%= qmovie[0]._fields[0].properties.year %></h3> 
-// <% };%>
 
 exports.post_rate = (req,res2,next) => {
     // update_rating
     const imdb_id = req.session.movie_id;
-    const rating = req.body.new_rating;
+    var rating = 1;
+    if (req.body.rate == "dislike"){
+        rating = 0;
+    }
     const userid = req.session.user;
-    const movie_data = req.session.movie_id;
     console.log(imdb_id);
     console.log(rating);
+    const user_obj = new db.User(userid);
+    const movie_obj = new db.Movie(imdb_id);
     
-    db.Movie.update_rating(imdb_id, userid, rating)
+    user_obj.remove_rating(imdb_id)
+    .then(res => {
+        user_obj.update_rating(imdb_id, rating)
         .then(res => {
-            res2.render('movie', {
-                pageTitle: 'Movie Data',
-                path: '/movie/?movie_id='+imdb_id,
-                movie_data: movie_data,
-                user_rating: rating
-            });
+            const out = res.records;
+            console.log(out);
+            user_obj.get_rating(imdb_id)
+            .then(res => {
+                const user_rating = res.records[0];
+                console.log(user_rating);
+                movie_obj.get_all_movies()
+                .then(res => {
+                    const movie_list = res.records;
+                    console.log(movie_list[0]._fields);
+                    console.log("movielist");
+
+                    res2.render('movie', {
+                        pageTitle: 'Movie Data',
+                        path: '/movie/?movie_id='+imdb_id,
+                        movie_data: req.session.movie_data,
+                        user_rating: user_rating,
+                        movie_list: movie_list,
+                        username: req.session.user,
+                        is_logged_in: req.session.is_logged_in
+                    });
+                })
+            })
         })
+    })
 };
 
--- controllers/recs.js
+++ controllers/recs.js
@@ -1,31 +1,35 @@
-const recs = require('../models/movie');
+const db = require('../models/movie');
 
 
 exports.get_recs = (req, res2, next) => {
     // get_recommendations
-    const recs_obj = new recs.Recommend(req.session.user);
-    recs_obj.get_recommendations(40)
+    const recs_obj = new db.Recommend(req.session.user);
+    if (req.session.user){
+        recs_obj.get_recommendations(40)
         .then(res =>{
-            if(req.session.user){
-                const all_reccos = res.rows;
-                res.session.all_recs = all_reccos;
-                res2.render('recs', {
-                    pageTitle: 'All Recommendations',
-                    path: '/recs/?username='+req.session.user,
-                    listnames: all_reccos,
-                    username: req.session.user,
-                    start_id: 0
-                });
-            }
-            else{
-                res.redirect('/login');
-            }
+            const all_recs = res.records;
+            res.session.all_recs = all_recs;
+            req.session.rec_size = 10;
+            console.log(all_recs[0]._fields);
+            console.log("recs list");
+
+            res2.render('recs', {
+                pageTitle: 'Recommendations',
+                path: '/recs/?username='+req.session.user,
+                all_recs: req.session.all_recs,
+                username: req.session.user,
+                start_id: 0,
+                rec_size: req.session.rec_size
+            });
         })
+    } else{
+        console.log("log in first");
+        res2.redirect('/login');
+    }
 };
 
 exports.post_submit = (req, res, next) => {
     // update_recs
-    // ????????????????????????????????????
     var preferences = req.body;
     for (var key of Object.keys(preferences)) {
         console.log(key + " -> " + preferences[key])
@@ -37,7 +41,6 @@ exports.post_submit = (req, res, next) => {
 
 exports.post_get_more = (req,res,next) =>{
     // update_recs
-    // get_more ????
     const all_reccos = res.session.all_reccos;
     const old_start_id = res.body.start_id;
     res.body.start_id = old_start_id+10;
